004edba28ec0f899125c984fbf29e377fe9b140a
Derek <derekalacy@gmail.com>
2025-05-04 22:48:19 -0400

    removed cached files

--- get_strat.py
+++
@@ -1,8 +0,0 @@
-from cfr_emulator import CFR, Node
-from pypokerengine.players import BasePokerPlayer
-
-CFR = CFR(
-    BasePokerPlayer(),
-    BasePokerPlayer(),
-)
-CFR.save_strategy("mccfr_strategy.json", load_fpath="saves/nodes.pkl")
